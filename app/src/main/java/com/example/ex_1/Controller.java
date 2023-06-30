a39fc8b9be06ac2f80d6f79d946a532f30b63bf1
isso0424 <isso04240129@gmail.com>
2023-06-30 14:56:45 +0900

    feat: add Controller signature

---
+++ app/src/main/java/com/example/ex_1/Controller.java
@@ -0,0 +1,20 @@
+package com.example.ex_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
+    private List<ToDO> list;
+
+    Controller() {
+        this.list = new ArrayList<>();
+    }
+
+    void add(ToDO todo) {
+
+    }
+
+    void delete(int index) {
+
+    }
+}
